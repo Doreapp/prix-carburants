8b3dcb229e69dd8eb14614be7faa6b38aaf61b39
AntoineMdn <antoine.mandin.am@gmail.com>
2022-05-17 19:15:23 +0200

    ✨ Fetch data from open data

--- prixcarburants/__main__.py
+++ prixcarburants/__main__.py
@@ -2,8 +2,22 @@
 Main entrypoint
 """
 
+import argparse
 import sys
 from typing import List
+
+from .fetch import DataFechter
+
+
+def build_cli_parser() -> argparse.ArgumentParser:
+    """Build and return Command Line Interface parser"""
+    parser = argparse.ArgumentParser(
+        "prixcarburants",
+        description="Program to fetch french fuel prices",
+        epilog="Built by Antoine MANDIN",
+    )
+    parser.add_argument("type", help="Type of data to download", choices=("instantaneous", "year"))
+    return parser
 
 
 def main(cli: List[str]):
@@ -11,7 +25,12 @@ def main(cli: List[str]):
     Main entrypoint
     :param cli: Command line arguments
     """
-    print(cli)
+    parser = build_cli_parser()
+    arguments = parser.parse_args(cli)
+    if arguments.type == "instantaneous":
+        DataFechter().download_instantaneous_data()
+    elif arguments.type == "year":
+        DataFechter().download_year_data()
 
 
 if __name__ == "__main__":
---
+++ prixcarburants/fetch.py
@@ -0,0 +1,47 @@
+"""
+Module containing utilities to fetch information from
+https://www.prix-carburants.gouv.fr/rubrique/opendata/ data
+"""
+
+import io
+import os
+import zipfile
+
+import requests
+
+INSTANTANEOUS_URL = "https://donnees.roulez-eco.fr/opendata/instantane"
+YEAR_URL = "https://donnees.roulez-eco.fr/opendata/annee"
+
+
+def download_zip(url, output_directory="tmp"):
+    """
+    Download a ZIP from ``url`` and extract it in ``output_directory``
+    """
+    response = requests.get(url)
+    with io.BytesIO(response.content) as stream, zipfile.ZipFile(stream) as zip_file:
+        zip_file.extractall(output_directory)
+
+
+class DataFechter:
+    """Fetcher of fuel prices"""
+
+    def __init__(self, output_directory=".tmp"):
+        """
+        :param output_directory: Direcotry to save data in
+        """
+        self.output_directory = output_directory
+        os.makedirs(self.output_directory, exist_ok=True)
+
+    def download_instantaneous_data(self):
+        """
+        Download newest data
+        Uses INSTANTANEOUS_URL
+        """
+        download_zip(INSTANTANEOUS_URL, self.output_directory)
+
+    def download_year_data(self):
+        """
+        Download latest year data
+        Uses YEAR_URL
+        """
+        download_zip(YEAR_URL, self.output_directory)
