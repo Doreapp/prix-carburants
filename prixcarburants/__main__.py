de1642f221702ec79ddaa39a163588207b34182b
AntoineMdn <antoine.mandin.am@gmail.com>
2022-05-26 11:04:56 +0200

    💄 Improve argument parsing

--- prixcarburants/__main__.py
+++ prixcarburants/__main__.py
@@ -16,8 +16,19 @@ def build_cli_parser() -> argparse.ArgumentParser:
         description="Program to fetch french fuel prices",
         epilog="Built by Antoine MANDIN",
     )
-    parser.add_argument(
-        "type", help="Type of data to download", choices=("instantaneous", "day", "year")
+    subparsers = parser.add_subparsers(
+        title="command", description="Program command", dest="command"
+    )
+    download_subparser = subparsers.add_parser(
+        "download", help="Download data from french Open Data"
+    )
+    download_subparser.add_argument(
+        "type",
+        help="Type of data to download. "
+        "[instantaneous]: most recent data, "
+        "[day]: today's data, "
+        "[year]: current year's data.",
+        choices=("instantaneous", "day", "year"),
     )
     return parser
 
@@ -29,14 +40,17 @@ def main(cli: Optional[List[str]] = None):
     """
     parser = build_cli_parser()
     arguments = parser.parse_args(cli)
-    if arguments.type == "instantaneous":
-        DataFechter().download_instantaneous_data()
-        sale_points = build_sale_points(".tmp/PrixCarburants_instantane.xml")
-        print(f"Found {len(sale_points)} sale points")
-    elif arguments.type == "day":
-        DataFechter().download_day_data()
-    elif arguments.type == "year":
-        DataFechter().download_year_data()
+    if arguments.command == "download":
+        if arguments.type == "instantaneous":
+            DataFechter().download_instantaneous_data()
+            sale_points = build_sale_points(".tmp/PrixCarburants_instantane.xml")
+            print(f"Found {len(sale_points)} sale points")
+        elif arguments.type == "day":
+            DataFechter().download_day_data()
+        elif arguments.type == "year":
+            DataFechter().download_year_data()
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
