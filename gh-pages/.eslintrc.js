e6d24e7929512b6e367ba0d36ec2365d7c4db8af
AntoineMdn <antoine.mandin.am@gmail.com>
2022-05-29 12:29:28 +0200

    ➕ Add ESLint

---
+++ gh-pages/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+}
